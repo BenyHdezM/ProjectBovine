1d6954bd54c8c7cc4a016c77c12e8216f876e8fc
Beny Hdez <beny_hm@hotmail.com>
2025-01-22 21:38:46 -0800

    Ag Grid lib added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { AgGridReact } from 'ag-grid-react'; // React Data Grid Component
+import { AllCommunityModule, ModuleRegistry } from 'ag-grid-community';
+
+// Register all Community features
+ModuleRegistry.registerModules([AllCommunityModule]);
 
 const App = () => {
+
+    const [rowData, setRowData] = useState([
+        { make: "Tesla", model: "Model Y", price: 64950, electric: true },
+        { make: "Ford", model: "F-Series", price: 33850, electric: false },
+        { make: "Toyota", model: "Corolla", price: 29600, electric: false },
+    ]);
+
+    // Column Definitions: Defines the columns to be displayed.
+    const [colDefs, setColDefs] = useState([
+        { field: "make" },
+        { field: "model" },
+        { field: "price" },
+        { field: "electric" }
+    ]);
+
     return (
-        <div>
-            <h1>Test Project Bovine</h1>
+        <div
+            // define a height because the Data Grid will fill the size of the parent container
+            style={{ height: 500 }}
+        >
+            <AgGridReact
+                rowData={rowData}
+                columnDefs={colDefs}
+            />
         </div>
     );
 };
