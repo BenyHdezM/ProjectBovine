9f6dff14fb6ad30779e27806ba4744c7ca27e983
Beny Hdez <beny_hm@hotmail.com>
2025-01-24 20:06:55 -0800

    enable sqlite

--- main.js
+++ main.js
@@ -19,13 +19,13 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
 
-// const db = require('./database.js');
+const db = require('./database.js');
 
-// db.serialize(() => {
-//     db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)'); //Create a Table
-    //This is an Example for Insert value on SQLite
+db.serialize(() => {
+    db.run('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT)'); //Create a Table
+    // This is an Example for Insert value on SQLite
     // db.run('INSERT INTO users (name) VALUES (?)', ['John Doe'], function(err) {
     //     if (err) console.error(err.message);
     //     else console.log(`Inserted row with ID: ${this.lastID}`);
     // });
-// });
+});
